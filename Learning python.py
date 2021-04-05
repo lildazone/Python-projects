214517bf8a5b578954a328a14f07b21d9e428221
lildazone <lildazone@gmail.com>
2021-04-05 11:41:46 +0100

    Add files via upload

---
+++ Learning python.py
@@ -0,0 +1,6 @@
+username = input('Input your name ')
+pswrd= input('Input your password ')
+if pswrd==('123'):
+    print('Your are login as',username,)
+else :
+    print('Your password is incorrect')
